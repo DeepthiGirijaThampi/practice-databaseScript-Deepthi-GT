b35bc8b4726aa13732fad014c16877a533f7661e
DeepthiGirijaThampi <deepthig01@gmail.com>
2026-01-14 20:20:39 -0600

    Complted the database script

--- databaseScript.sql
+++ databaseScript.sql
@@ -1,38 +1,49 @@
 -- Start Transaction
 START TRANSACTION;
 -- Drop tables if exists
-DROP TABLE IF EXISTS users,books,checkouts;
+DROP TABLE IF EXISTS restaurants,customer,order,royalty,menu,inventory;
 -- Table users
-CREATE TABLE users(
-user_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-name VARCHAR(45) NOT NULL,
-email VARCHAR(50) NULL DEFAULT 'N/A'
+CREATE TABLE restaurants (
+restaurant_id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(100) NOT NULL,
+address VARCHAR(200),
+phone_number VARCHAR(20)
 );
 
--- Table books
-CREATE TABLE books(
-book_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-title VARCHAR(100) NOT NULL,
-author VARCHAR(100)
+-- Table customer
+CREATE TABLE customer(
+phone_number INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+name VARCHAR(100) NOT NULL,
+email VARCHAR(100)
 );
--- Table checkouts
-CREATE TABLE checkouts(
-checkout_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-user_id INT NOT NULL REFERENCES users(user_id),
-book_id INT NOT NULL REFERENCES books(book_id),
-checkout_date DATE
+--Table royalty
+CREATE TABLE royalty(
+royalty_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+customer_id INT REFERENCES customer(phone_number),
+category VARCHAR(15),
+points_left int
 );
--- Insert into users
-INSERT INTO users (name, email) VALUES
-('Alice', 'alice@email.com'),
-('Bob', 'bob@email.com');
--- Insert into books
-INSERT INTO books (title, author) VALUES
-('Harry Potter', 'J.K. Rowling'),
-('The Secret', 'Rhonda Byrne');
--- Insert into checkouts
-INSERT INTO checkouts (user_id, book_id, checkout_date) VALUES
-(1, 1, '2024-01-01'),
-(2, 2, '2024-01-05');
+-- Table orders
+CREATE TABLE orders (
+order_id INT PRIMARY KEY AUTO_INCREMENT,
+customer_id INT NOT NULL REFERENCES customer(phone_number),
+restaurant_id INT NOT NULL REFERENCES restaurants(restaurant_id),
+order_date VARCHAR(20),
+);
+-- Table menu
+CREATE TABLE menu (
+menu_id INT PRIMARY KEY AUTO_INCREMENT,
+restaurant_id INT NOT NULL,
+name VARCHAR(20),
+FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id)
+);
+-- Table inventory
+CREATE TABLE inventory(
+restaurant_id INT PRIMARY KEY AUTO_INCREMENT,
+item_name VARCHAR(28),
+stock INT,
+unit VARCHAR(5)
+);
 
-COMMIT;
+ROLLBACK;
+--COMMIT;
